d7d635ad40b50b6e51ce49fd666b92d276b1558c
Error1417 <101829080+Error1417@users.noreply.github.com>
2022-12-09 20:25:31 -0800

    Add files via upload

---
+++ CIS17A_Project2_Restart/main.cpp
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Ismael Perez
+ * Purpose:
+ * 
+ *
+ * 
+ * Created on December, 2022, 2:27 PM
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include "BlackJack.h"
+
+using namespace std;
+
+
+// Main function (driver).
+int main() {
+	Game g;
+        g.play();
+	
+	return 0;
+}
